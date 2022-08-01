cba197626ab211c30085ca37b632044b1248a144
Usuário SENAC <humberto.cgomes17@gmail.com>
2022-08-01 10:12:21 -0300

    Java

--- Java/01.08.2022/Programa08.java
+++ Java/01.08.2022/Programa08.java
@@ -1,3 +1,42 @@
+import java.util.Scanner;
 public class Programa08 {
+     public static void main(String[] args) {
+     try (Scanner ler = new Scanner(System.in)) {
+        int num1;
+        int num2;
+        int ope;
+        int num;
+        String res;
+        res = "";
+        
+   System.out.printf("Primeiro numero: ");
+   num1 = ler.nextInt();
+   System.out.printf("Segundo numero: ");
+   num2 = ler.nextInt();
+   System.out.printf("Operação: 1 = Adição/ 2 = Subtração/ 3 = Multiplicação/ 4 = Divisão ");
+   ope = ler.nextInt();
+        
+   switch (ope) {
+   case 1:
+   num = num1 + num2;
+   break;
+   case 2:
+   num = num1 - num2;
+   break;
+   case 3:
+   num = num1 * num2;
+   break;
+   case 4:
+        num = num1 / num2;    
+        break;
+        default:
+        num = 0;
+        res = "Operação inválida";
+        break;
+            }
+        System.out.print(num + res);
+    }
     
-}
+    System.exit(0);
+        }
+    }
--- Java/01.08.2022/Programa09.java
+++ Java/01.08.2022/Programa09.java
@@ -1,3 +1,28 @@
+import java.util.Scanner;
 public class Programa09 {
-    
+    public static void main(String[] args) {
+        try (Scanner ler = new Scanner(System.in)) {
+            String  nt;
+            String res;
+            res = "";
+            System.out.printf("Qual a sigla: ");
+            nt = ler.next();
+            switch (nt) {
+                case "ND":
+                    res = "Não desenvolvido";
+                break;
+                case "ED":
+                    res = "Em desenvolvimento";
+                break;
+                case "PD":
+                    res = "Plenamente desenvolvido";
+                break;
+                default:
+                    res = "Sigla inválida";
+                    break;
+            }
+            System.out.print(res);
+        }
+        System.exit(0);
+    }
 }
