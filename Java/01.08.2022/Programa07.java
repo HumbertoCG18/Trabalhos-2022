5bc84560032b034854e78a2b887e4582957863cd
Usuário SENAC <humberto.cgomes17@gmail.com>
2022-08-01 10:04:15 -0300

    Java

--- Java/01.08.2022/Programa07.java
+++ Java/01.08.2022/Programa07.java
@@ -1,3 +1,62 @@
+import java.util.Scanner;
 public class Programa07 {
+
+        public static void main(String[] args) {
+            Scanner in = new Scanner(System.in);
     
-}
+            int mes;
+    
+            System.out.println("Informe o código do produto: ");
+            mes = in.nextInt();
+    
+            switch (mes) {
+                case 1:
+                    System.out.println("Alimento não-perecível");
+                    break;
+                case 2:
+                    System.out.println("Alimento perecível");
+                    break;
+                case 3:
+                    System.out.println("Alimento perecível");
+                    break;
+                case 4:
+                    System.out.println("Alimento perecível");
+                    break;
+                case 5:
+                    System.out.println("Vestuário");
+                    break;
+                case 6:
+                    System.out.println("Vestuário");
+                    break;
+                case 7:
+                    System.out.println("Higiene Pessoal");
+                    break;
+                case 8:
+                    System.out.println("Limpeza e Utensílios Domésticos");
+                    break;
+                case 9:
+                    System.out.println("Limpeza e Utensílios Domésticos");
+                    break;
+                case 10:
+                    System.out.println("Limpeza e Utensílios Domésticos");
+                    break;
+                case 11:
+                    System.out.println("Limpeza e Utensílios Domésticos");
+                    break;
+                case 12:
+                    System.out.println("Limpeza e Utensílios Domésticos");
+                    break;
+                case 13:
+                    System.out.println("Limpeza e Utensílios Domésticos");
+                    break;
+                case 14:
+                    System.out.println("Limpeza e Utensílios Domésticos");
+                    break;
+                case 15:
+                    System.out.println("Limpeza e Utensílios Domésticos");
+                    break;
+            }
+            
+            in.close();
+        }
+    }
--- Java/01.08.2022/Programa10.java
+++ Java/01.08.2022/Programa10.java
@@ -1,3 +1,20 @@
+import java.util.Scanner;
 public class Programa10 {
-    
+
+    public static void main(String[] args) {
+        try (Scanner scan = new Scanner(System.in)) {
+            int counter = 0;
+            float[] precos = new float[3];
+            int smallest = 0;
+            while (counter < 3) {
+                System.out.printf("Qual o preço do produto %d?\n", counter + 1);
+                precos[counter] = scan.nextFloat();
+                if (precos[counter] < precos[smallest]) {
+                    smallest = counter;
+                }
+                counter++;
+            }
+            System.out.printf("Você deve comprar o produto %d.\n", smallest + 1);
+        }
+    }
 }
