474538b13de2b5ea81640f57dc295f8199102967
Usuário SENAC <humberto.cgomes17@gmail.com>
2022-08-01 08:29:54 -0300

    Update Programa01.java

--- Java/01.08.2022/Programa01.java
+++ Java/01.08.2022/Programa01.java
@@ -2,15 +2,15 @@ import java.util.Scanner;
 
 public class Programa01 {
     public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
-        
-        System.out.println("Digite o seu nome: ");
-        String nome = in.nextLine();
-        
-            System.out.println("Informe a sua idade: ");
-            int idade = in.nextInt();
+        try (Scanner in = new Scanner(System.in)) {
+            System.out.println("Digite o seu nome: ");
+            String nome = in.nextLine();
+            
+                System.out.println("Informe a sua idade: ");
+                int idade = in.nextInt();
 
-            System.out.println("Nome: "+nome);
-            System.out.print("Idade: "+idade+" anos" );
+                System.out.println("Nome: "+nome);
+                System.out.print("Idade: "+idade+" anos" );
+        }
     }
 }
