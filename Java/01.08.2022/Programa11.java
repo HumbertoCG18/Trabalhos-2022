65473da23791f2ce41eafa9660223c5e01f48e26
Humberto Corrêa <humberto.cgomes17@gmail.com>
2022-08-01 21:39:54 -0300

    Update Programa11.java

--- Java/01.08.2022/Programa11.java
+++ Java/01.08.2022/Programa11.java
@@ -1,3 +1,28 @@
+import java.util.Scanner;
 public class Programa11 {
-    
+    public static void main(String [] args) {
+
+    try (Scanner entrada = new Scanner (System.in)) {
+        char opcao;
+
+        System.out.println("Em qual turno você estuda? (M = Matuino, V= Vespertino ou N = Noturno)");
+        opcao=entrada.next() .charAt(0);
+
+        if(opcao == 'm' || opcao == 'M' ){
+            System.out.println("Bom Dia!");
+        }
+
+        else if(opcao == 'v' || opcao == 'V' ){
+            System.out.println("Boa Tarde!");
+        }
+
+        else if(opcao == 'n' || opcao == 'N' ){
+            System.out.println("Boa Noite!");
+        }
+
+        else{
+            System.out.println("Valor Inválido!");
+        }
+    }
+}
 }
