ccc6aa8aa00580bfac7661e6ad9bcc283df9e63a
HumbertoCG18 <humberto.cgomes17@gmail.com>
2022-08-03 03:33:08 -0900

    java

--- Java/03.08.2022/Programa02.java
+++ Java/03.08.2022/Programa02.java
@@ -1,42 +1,43 @@
 import java.util.Scanner;
 public class Programa02 {
     public static void main(String[] args) {
-        Scanner ent = new Scanner(System.in);
-        int nota1, nota2, nota3;
-        int media, i, contAluno = 0;
-        
-        for(i = 0; i < 3; i++){
+        try (Scanner ent = new Scanner(System.in)) {
+            int nota1, nota2, nota3;
+            int media, i, contAluno = 0;
             
-            contAluno++;
-            System.out.println("Aluno " + contAluno + ", digite sua 1ª nota");
-            nota1 = ent.nextInt();
-            
-            System.out.println("Aluno " + contAluno + ", digite sua 2ª nota");
-            nota2 = ent.nextInt();
-            
-            System.out.println("Aluno " + contAluno + ", digite sua 3ª nota");
-            nota3 = ent.nextInt();
-            
-            media = (nota1 + nota2 + nota3) / 3;
-            System.out.println("A média do aluno " + contAluno + " é " + media);
-            
-            if( (media >= 0) && (media <4) ){
-                System.out.println("Nota E");
-            } 
-            
-            else if(media < 5){
-                System.out.println("Nota D");
-            } 
-            
-            else if(media < 7){
-                System.out.println("Nota C");
-            } 
-            
-            else if(media < 8){
-                System.out.println("Nota B");
-            } 
-            else if(media <= 10){
-                System.out.println("Nota A");
+            for(i = 0; i < 3; i++){
+                
+                contAluno++;
+                System.out.println("Aluno " + contAluno + ", digite sua 1ª nota");
+                nota1 = ent.nextInt();
+                
+                System.out.println("Aluno " + contAluno + ", digite sua 2ª nota");
+                nota2 = ent.nextInt();
+                
+                System.out.println("Aluno " + contAluno + ", digite sua 3ª nota");
+                nota3 = ent.nextInt();
+                
+                media = (nota1 + nota2 + nota3) / 3;
+                System.out.println("A média do aluno " + contAluno + " é " + media);
+                
+                if( (media >= 0) && (media <4) ){
+                    System.out.println("Nota E");
+                } 
+                
+                else if(media < 5){
+                    System.out.println("Nota D");
+                } 
+                
+                else if(media < 7){
+                    System.out.println("Nota C");
+                } 
+                
+                else if(media < 8){
+                    System.out.println("Nota B");
+                } 
+                else if(media <= 10){
+                    System.out.println("Nota A");
+                }
             }
         }       
     }
