18c9673b951f500cd20b01f7e80b4b3993ea82eb
HumbertoCG18 <humberto.cgomes17@gmail.com>
2022-08-03 02:47:21 -0900

    Update Programa1.java

--- Java/03.08.2022/Programa1.java
+++ Java/03.08.2022/Programa1.java
@@ -1,3 +1,17 @@
 public class Programa02 {
+    public static void main(String[] args) {
     
+    int inicio,termino;
+    String nome;
+
+    nome= "Humberto";
+    inicio = 15;
+    termino = 30;
+
+    if ( ){
+    for (int contador = 0; contador < 5 ; contador++){
+        System.out.println(nome+" "+contador);
+            }
+        }
+    }
 }
