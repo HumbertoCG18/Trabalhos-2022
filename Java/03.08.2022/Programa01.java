254316500f04d724fedbe0b5338bd316350a37df
HumbertoCG18 <humberto.cgomes17@gmail.com>
2022-08-03 03:25:27 -0900

    Java

--- Java/03.08.2022/Programa01.java
+++ Java/03.08.2022/Programa01.java
@@ -1,14 +1,13 @@
-public class Programa02 {
+public class Programa01 {
     public static void main(String[] args) {
     
-    int inicio,termino;
+    int inicio;
     String nome;
 
     nome= "Humberto";
     inicio = 15;
-    termino = 30;
 
-    if ( ){
+    if( inicio>10){
     for (int contador = 0; contador < 5 ; contador++){
         System.out.println(nome+" "+contador);
             }
