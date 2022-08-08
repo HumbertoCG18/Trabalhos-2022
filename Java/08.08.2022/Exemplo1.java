76d5e82060f363a4fa392f3fe0f68f540a1004a6
Usuário SENAC <humberto.cgomes17@gmail.com>
2022-08-08 08:13:51 -0300

    Create Exemplo1.java

---
+++ Java/08.08.2022/Exemplo1.java
@@ -0,0 +1,11 @@
+public class Exercicio 1{
+    public static void main(String[] args){
+        for(int i = 1; i<= 5; i++){
+            System.out.println("("+i+")");
+            for (int x = 1; x <= 5; x++){
+                System.out.println(+i" "+x);
+            }
+            System.out.println("---");
+        }
+    }
+}
